b240e6cb80df65f623fdd392a769f68d32d41538
Cliffsz <marc.s.mogensen@gmail.com>
2019-03-02 16:44:23 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mybar/Drinks.java
@@ -0,0 +1,20 @@
+package com.example.mybar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class Drinks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drinks);
+        String[] array = getResources().getStringArray(R.array.Drinks_array);
+        ArrayAdapter<String> DrinksAdapter= new ArrayAdapter<>(
+                this, android.R.layout.simple_list_item_1, array);
+        ListView MyListView = findViewById(R.id.recycler_drinks);
+        MyListView.setAdapter(DrinksAdapter);
+    }
+}
---
+++ app/src/main/java/com/example/mybar/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.mybar;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void map (View v) {
+        Intent i = new Intent(this, Maps.class);
+        startActivity(i);
+    }
+    public void about (View v) {
+        Intent i = new Intent(this, about.class);
+        startActivity(i);
+    }
+    public void contact (View v) {
+        Intent i = new Intent(this, contact.class);
+        startActivity(i);
+    }
+    public void drinks (View v) {
+        Intent i = new Intent(this, Drinks.class);
+        startActivity(i);
+    }
+
+}
+
